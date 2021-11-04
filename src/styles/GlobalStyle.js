f0c6d17d6c056225e86f9e35a55ed104865c4848
vishalpatil12014017 <87421912+vishalpatil12014017@users.noreply.github.com>
2021-11-04 16:52:04 +0530

    styles

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,197 @@
+import {createGlobalStyle} from 'styled-components';
+
+
+const GlobalStyle = createGlobalStyle`
+
+
+.light-theme{
+    --primary-color: #007bff;
+    --primary-color-light: #057FFF;
+    --secondary-color: #ff7675;
+    --background-dark-color: #F1F1F1;
+    --background-dark-grey: #e4e4e4;
+    --border-color: #cbced8;
+    --background-light-color: #F1F1F1;
+    --background-light-color-2: rgba(3,127,255,.3);
+    --white-color: #151515;
+    --font-light-color: #313131;
+    --font-dark-color: #313131;
+    --font-dark-color-2: #151515;
+    --sidebar-dark-color: #E4E4E4;
+    --scrollbar-bg-color: #383838;
+    --scrollbar-thump-color: #6b6b6b;
+    --scrollbar-track-color: #383838;
+}
+.dark-theme{
+    --primary-color: #007bff;
+    --primary-color-light: rgba(5,127,255,.7);
+    --secondary-color: #6c757d;
+    --background-dark-color: #10121A;
+    --background-dark-grey: #191D2B;
+    --border-color: #2e344e;
+    --background-light-color: #F1F1F1;
+    --background-light-color-2: rgba(3,127,255,.3);
+    --white-color: #FFF;
+    --font-light-color: #a4acc4;
+    --font-dark-color: #313131;
+    --font-dark-color-2: #151515;
+    --sidebar-dark-color: #191D2B;
+    --scrollbar-bg-color: #383838;
+    --scrollbar-thump-color: #6b6b6b;
+    --scrollbar-track-color: #383838;
+}
+
+.orange-theme{
+    --primary-color: #fa5b0f !important;
+    --primary-color-light: rgba(250,91,15,0.8) !important;
+    --background-light-color-2: rgba(250,91,15,0.2) !important;
+}
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    font-family: 'Nunito', sans-serif;
+    font-size: 1.1rem;
+    
+}
+
+body{
+    background-color: var(--background-dark-color);
+    color: var(--font-light-color);
+    transition: all .4s ease-in-out;
+      overflow-x: hidden;
+}
+
+body::-webkit-scrollbar{
+    width: 9px;
+    background-color: #383838;
+}
+body::-webkit-scrollbar-thumb{
+    border-radius: 10px;
+    background-color:var(--primary-color);
+}
+body::-webkit-scrollbar-track{
+    border-radius: 10px;
+    background-color: #383838;
+}
+
+textarea{
+    max-width: 100%;
+}
+a{
+    font-family: inherit;
+    color: inherit;
+    font-size: inherit;
+    font-size: 1rem;
+}
+.App{
+    transition: 1500ms ease-ease-in-out;
+}
+h1{
+    font-size: 4rem;
+    color: var(--white-color);
+    span{
+        font-size: 4rem;
+        @media screen and (max-width: 502px){
+            font-size: 3rem
+        }
+    }
+    @media screen and (max-width: 502px){
+        font-size: 3rem
+    }
+}
+
+.colors{
+    position: fixed;
+    right: 0;
+    top: 150px;
+    z-index: 100;
+    width: 40px;
+    background-color: transparent;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+   
+}
+.colors span{
+    display: inline-block;
+    width: 30px;
+    margin-top: 10px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: #72b626;
+    cursor: pointer;
+}
+.colors span:nth-child(1){
+    background-color: #fa5b0f;
+}
+.colors span:nth-child(2){
+    background-color: rgb(0,123,255);
+}
+span{
+    color: var(--primary-color);
+}
+h6{
+    color: var(--white-color);
+    font-size: 1.2rem;
+    padding-bottom: .6rem;
+}
+
+//Utilities
+.u-margin-bottom{
+    margin-bottom: 4rem;
+}
+
+
+//Floting Toggler
+.light-dark-mode{
+    position: fixed;
+    right: -30px;
+    top: 460px;
+    border-radius: 10px;
+    background-color: var(--background-light-color-2);
+    width: 6.5rem;
+    height: 2.5rem;
+    transform: rotate(90deg);
+    z-index: 15;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg{
+      display: flex;
+      align-items: center;
+      font-size: 1.7rem;
+      color: var(--white-color);
+    }
+  }
+
+  //Nav Toggler
+  .ham-burger-menu{
+      position: sticky;
+      right: 3%;
+    text-align: right;
+      /* top: 3%;  */
+      display: none;
+      z-index: 15;
+      svg{
+          font-size: 3rem;
+      }
+    }
+
+    .nav-toggle{
+        transform: translateX(0);
+        z-index: 20;
+    }
+  @media screen and (max-width:1200px){
+    .ham-burger-menu{
+        display: block;
+    }
+  }
+//Global Media Queries
+
+
+
+`;
+
+export default GlobalStyle;
