dc5d56b29cc78c3500b181eeaf18a7ede6e6cc6b
vishalpatil12014017 <vishalpatil@Vishals-MacBook-Air.local>
2023-05-31 16:11:10 +0530

    done

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -2,6 +2,16 @@ import Traveltech from  '../img/blogs/Traveltech.png';
 import zoomcar from  '../img/blogs/zoomcar.png';
 import Discovery from  '../img/blogs/3.png';
 import SQL from  '../img/blogs/sql.jpeg';
+import InbuiltFeatures from  '../img/blogs/mysql.jpeg';
+import mysqlPartition from  '../img/blogs/mysqlPartition.png';
+import mysqlTransactions from  '../img/blogs/mysqlTransactions.jpeg';
+import mysqlStoredProceduresAndFunctions from  '../img/blogs/mysqlStoredProceduresAndFunctions.png';
+import mysqlTriggers from  '../img/blogs/mysqlTriggers.jpeg';
+import mysqlReplication from  '../img/blogs/mysqlReplication.jpeg';
+
+
+
+
 const blogs = [
     {
         id: 1,
@@ -35,6 +45,54 @@ const blogs = [
         month: 'April 2022',
         src:"https://medium.com/@Vishal_Patil/sql-queries-b5a8f4ccd9a1",
         image: SQL
+    },
+    {
+        id: 5, 
+        title: 'MySQL Inbuilt Features',
+        date: "26",
+        month: 'April 2023',
+        src:"https://medium.com/@Vishal_Patil/mysql-inbuilt-features-b055ad6d3d2",
+        image: InbuiltFeatures
+    },
+    {
+        id: 6, 
+        title: 'MySQL Triggers',
+        date: "26",
+        month: 'April 2023',
+        src:"https://medium.com/@Vishal_Patil/mysql-triggers-274f20d539c8",
+        image: mysqlTriggers
+    },
+    {
+        id: 7, 
+        title: 'Stored Procedures and MySQL functions',
+        date: "26",
+        month: 'April 2023',
+        src:"https://medium.com/@Vishal_Patil/stored-procedures-and-mysql-functions-96d0b82fbbef",
+        image: mysqlStoredProceduresAndFunctions
+    },
+    {
+        id: 8, 
+        title: 'Transactions in MySQL',
+        date: "26",
+        month: 'April 2023',
+        src:"https://medium.com/@Vishal_Patil/transactions-in-mysql-2bdc865bd560",
+        image: mysqlTransactions
+    },
+    {
+        id: 9, 
+        title: 'Mysql Replication',
+        date: "26",
+        month: 'April 2023',
+        src:"https://medium.com/@Vishal_Patil/mysql-replication-ecf664b35277",
+        image: mysqlReplication
+    },
+    {
+        id: 10, 
+        title: 'Mysql Partitioning',
+        date: "26",
+        month: 'April 2023',
+        src:"https://medium.com/@Vishal_Patil/partitioning-356411b0060a",
+        image: mysqlPartition
     }
 ]
 
